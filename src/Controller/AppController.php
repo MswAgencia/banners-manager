387daaf3d71e32fadf1b0c018828538d2da19e4e
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2016-02-18 17:49:14 -0200

    removido codigo desnecessário

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,14 +6,5 @@ use ControlPanel\Controller\AppController as BaseController;
 use Cake\Core\Configure;
 
 class AppController extends BaseController {
-	/**
-	 * Método responsável pela inicialização e atribuição de alguns valores após o bootstrap para o correto funcionamento do plugin.
-	 *
-	 * @return void
-	 */
-	public function initialize(){
-		parent::initialize();
-		$this->theme = Configure::read('WebImobApp.Plugins.BannersManager.Settings.Template.theme');
-		$this->layout = Configure::read('WebImobApp.Plugins.BannersManager.Settings.Template.layout');
-	}
+	
 }
