839961c59e20c2a62c09f4e47b9f9670d80766f8
Bruno Schmidt <bruno-schmidt@users.noreply.github.com>
2016-02-23 16:14:25 -0300

    removido configurações desnecessarias

--- config/default_settings.php
+++ config/default_settings.php
@@ -4,10 +4,6 @@ use Cake\Core\Configure;
 return [
 	'MswAgencia.Plugins.BannersManager.Settings' => [
 		'General' => ['display_panel_menu' => true],
-		'Template' => [
-			'layout' => Configure::read('MswAgencia.Plugins.ControlPanel.Settings.Template.layout'),
-			'theme' => Configure::read('MswAgencia.Plugins.ControlPanel.Settings.Template.theme')
-			],
 		'Options' => [
 			'use_name' => true,
 			'use_link' => false,
